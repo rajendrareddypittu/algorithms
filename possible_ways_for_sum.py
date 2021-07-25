453f7b91ddafd3c9fa777d201c63eeccb5379fff
rajendrareddypittu <74333926+rajendrareddypittu@users.noreply.github.com>
2021-07-25 16:54:42 -0700

    Update possible_ways_for_sum.py

--- possible_ways_for_sum.py
+++ possible_ways_for_sum.py
@@ -1,4 +1,7 @@
-def possibleways_for_sum(options,sum,new_sum,way,oldel):
+def possibleways_for_sum(ind,options,sum,new_sum,way,oldel):
+	if ind>len(options):
+		print("ind>op and ind=",ind)
+		return
 	if new_sum>sum:
 		print("NA")
 		return
@@ -6,17 +9,22 @@ def possibleways_for_sum(options,sum,new_sum,way,oldel):
 		print("way :",list(way))
 		return
 	if new_sum<sum:
-		#print("rem_sum<sum")
+		print("rem_sum<sum")
 		#options.remove(oldel)
-		for ele in options:
+		for i in range(ind,len(options)):
+					print("i",i)
+					ele=options[i]
+			
 					newop=options.copy()
-					newop.remove(ele)
-					possibleways_for_sum(newop[:],sum,new_sum+int(ele),way+str(ele),ele)
-		return
+					#newop.remove(ele)
+					#print(i,sum,new_sum+int(ele),list(way+str(ele)),ele)
+					possibleways_for_sum(i+1,newop[:],sum,new_sum+int(ele),way+str(ele),ele)
+		
+	return	
 		
 options=[1,2,3]
-sum=5
+sum=4
 ele=''
 way=''
-possibleways_for_sum(options[:],sum,0,way,ele)
+possibleways_for_sum(0,options[:],sum,0,way,ele)
 print(way)
